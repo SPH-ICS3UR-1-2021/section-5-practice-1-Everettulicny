d088ce5ef4c414fe3155bcd9ec224baa41233db2
Everettulicny <74618971+Everettulicny@users.noreply.github.com>
2021-01-07 13:17:10 -0500

    Finished

--- src/com/example/ColorRange.java
+++ src/com/example/ColorRange.java
@@ -9,7 +9,30 @@ import java.util.Scanner;
 
 public class ColorRange {
 
-    public static void main(String args[]) {
-        
+    public static void main(String args[]) {  
+        Scanner in = new Scanner(System.in);
+        System.out.println("Give me a number");
+        int num = in.nextInt();
+        if (num >=380 && num <=450){
+            System.out.println("Your color is Violet");
         }
+        else if (num >=450 && num <=495){
+            System.out.println("Your color is Blue");
+        }
+        else if (num >=495 && num <=570){
+            System.out.println("Your color is Green");
+        }
+        else if (num >=570 && num <=590){
+            System.out.println("Your color is Yellow");
+        }
+        else if (num >=590 && num <=620){
+            System.out.println("Your color is Orange");
+        }
+        else if (num >=620 && num<=750){
+            System.out.println("Your color is Red");
+        }
+        else
+            System.out.println("The entered wavelenght is not on the visible spectrum");
+        }
+        
     }
